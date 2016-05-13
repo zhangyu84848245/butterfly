1d4de10c29020fdaa27dac2e0c5782fc14090194
zhangyu <zhangyu84848245@163.com>
2016-05-13 15:29:50 +0800

    public void serviceInit() { 		shutdownHook = new Thread() { 			public void run() { 				ClientBootstrap.this.stop(); 			} 		}; 		Runtime.getRuntime().addShutdownHook(shutdownHook); 		super.serviceInit(); 	}
    	public void serviceStop() {
    		super.serviceStop();
    		// stop zookeeper
    		getRegistryBootstrap().stop();
    	}

--- src/main/java/org/fantasy/bean/bootstrap/client/ClientBootstrap.java
+++ src/main/java/org/fantasy/bean/bootstrap/client/ClientBootstrap.java
@@ -2,10 +2,12 @@ package org.fantasy.bean.bootstrap.client;
 
 import org.fantasy.bean.annotation.Consumer;
 import org.fantasy.bean.bootstrap.ConfigurableResourceBootstrap;
+import org.fantasy.bean.bootstrap.server.ServerBootstrap;
 import org.fantasy.util.Constant;
 
 public class ClientBootstrap extends ConfigurableResourceBootstrap {
 
+	private volatile Thread shutdownHook;
 	public ClientBootstrap() {
 		this(Constant.DEFAULT_CONFIG_LOCATION);
 	}
@@ -15,4 +17,21 @@ public class ClientBootstrap extends ConfigurableResourceBootstrap {
 		setAnnotationClass(Consumer.class);
 	}
 
+	public void serviceInit() {
+		shutdownHook = new Thread() {
+			public void run() {
+				ClientBootstrap.this.stop();
+			}
+		};
+		Runtime.getRuntime().addShutdownHook(shutdownHook);
+		super.serviceInit();
+	}
+
+	public void serviceStop() {
+		super.serviceStop();
+		// stop zookeeper
+		getRegistryBootstrap().stop();
+	}
+
+	
 }
