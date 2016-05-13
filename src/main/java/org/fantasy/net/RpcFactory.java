9d18c0177177a20f06305a2c4caf0cff6e6d05f4
zhangyu <zhangyu84848245@163.com>
2016-05-13 15:37:36 +0800

    Closeable

--- src/main/java/org/fantasy/net/RpcFactory.java
+++ src/main/java/org/fantasy/net/RpcFactory.java
@@ -1,8 +1,10 @@
 package org.fantasy.net;
 
+import java.io.Closeable;
+
 import org.fantasy.net.RpcFactoryProvider.Endpoint;
 
-public interface RpcFactory {
+public interface RpcFactory extends Closeable {
 
 	public <T> T create(String bindAddress, int port, Endpoint endpoint);
 }
